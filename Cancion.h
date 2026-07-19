796afc92fca4f26075fc18241614f7e28fddd260
allan_jaxr <allanarmas06@gmail.com>
2026-07-19 12:52:26 -0500

    Add constructor to Cancion structure
    Added a constructor for the Cancion structure.

--- Cancion.h
+++ Cancion.h
@@ -5,12 +5,14 @@
 
 using namespace std;
 
+//Se crea la estructura de la cancion en C++
 struct Cancion {   
 	int id;   
 	string nombre; 
 	string artista;
 	string genero;
-	
+
+//Se crea el constructor de la cancion
 	string mostrar() const {
 		ostringstream oss;
 		oss << "ID: " << id << ", Nombre: " << nombre << ", Artista: "
